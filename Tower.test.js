6a0021330e587f9b54d232a704fd6ccc75999880
Arseniy Bikulov <arsenybikulov@gmail.com>
2022-03-28 01:06:51 +0700

    Task 1 commit 2

--- Tower.test.js
+++ Tower.test.js
@@ -1,57 +1,98 @@
 const {Build} = require('./Tower')
 
 describe("Valid input", () => {
+    it.each([
+      [1, ["*"]],
 
-    test('Should return three-storey tower', () => {
-        expect(Build(3)).toEqual(["  *  "," *** ","*****"])
-    })
-    
-    test('Should return five-storey tower', () => {
-        expect(Build(5)).toEqual(["    *    ","   ***   ","  *****  "," ******* ","*********"])
-    })
+      [3, ["  *  ",
+           " *** ",
+           "*****"]],
 
-})
+      [5, ["    *    ",
+           "   ***   ",
+           "  *****  ",
+           " ******* ",
+           "*********"]],
+
+      [15, ["              *              ",
+            "             ***             ",
+            "            *****            ",
+            "           *******           ",
+            "          *********          ",
+            "         ***********         ",
+            "        *************        ",
+            "       ***************       ",
+            "      *****************      ",
+            "     *******************     ",
+            "    *********************    ",
+            "   ***********************   ",
+            "  *************************  ",
+            " *************************** ",
+            "*****************************"]],
+
+      [25, ["                        *                        ",
+            "                       ***                       ",
+            "                      *****                      ",
+            "                     *******                     ",
+            "                    *********                    ",
+            "                   ***********                   ",
+            "                  *************                  ",
+            "                 ***************                 ",
+            "                *****************                ",
+            "               *******************               ",
+            "              *********************              ",
+            "             ***********************             ",
+            "            *************************            ",
+            "           ***************************           ",
+            "          *****************************          ",
+            "         *******************************         ",
+            "        *********************************        ",
+            "       ***********************************       ",
+            "      *************************************      ",
+            "     ***************************************     ",
+            "    *****************************************    ",
+            "   *******************************************   ",
+            "  *********************************************  ",
+            " *********************************************** ",
+            "*************************************************"]],
+    ])(
+      `Should return %i-storey tower`,
+      (x, result) => {
+        expect(Build(x)).toEqual(result);
+      }
+    );
+  });
+
+
 
 describe("Invalid input value", () => {
+    test.each([
+      {x: -4, name: 'Should return input value error(negative number)', res: "Количество этажей не может быть меньше единицы"},
+      {x: 0, name: 'Should return input value error(zero)',res: "Количество этажей не может быть меньше единицы"},
+      {x: 2.5,name: 'Should return input value error(float)',res: "Количество этажей не может быть дробным числом"},
+    ])(
+      `$name`,
+      ({x, name, res}) => {
+        expect(Build(x)).toEqual(res);
+      }
+    );
+  });   
 
-    test('Should return input value error(negative number)', () => {
-        expect(Build(-4)).toBe("Количество этажей не может быть меньше единицы")
-    })
 
-    test('Should return input value error(zero)', () => {
-        expect(Build(0)).toBe("Количество этажей не может быть меньше единицы")
-    })
+  describe("Invalid input type", () => {
+    test.each([
+      {x:"Three", name: 'Should return input type error(string)', res: "На вход принимается только число"},
+      {x: '3', name: 'Should return input type error(string) v2',res: "На вход принимается только число"},
+      {x: ["3"],name: 'Should return input type error(array)',res: "На вход принимается только число"},
+      {x: null ,name: 'Should return input type error(null)',res: "На вход принимается только число"},
+      {x: undefined,name: 'Should return input type error(undefined)',res: "На вход принимается только число"},
+      {x: "",name: 'Should return input type error(empty string)',res: "На вход принимается только число"},
+    ])(
+      `$name`,
+      ({x, name, res}) => {
+        expect(Build(x)).toEqual(res);
+      }
+    );
+  });   
 
-    test('Should return input value error(float)', () => {
-        expect(Build(2.5)).toBe("Количество этажей не может быть дробным числом")
-    })
 
-})
-
-describe("Invalid input type", () => {
-
-    test('Should return input type error(string)', () => {
-        expect(Build("Three")).toBe("На вход принимается только число")
-    })
-
-    test('Should return input type error(string) v2', () => {
-        expect(Build('3')).toBe("На вход принимается только число")
-    })
-
-    test('Should return input type error(array)', () => {
-        expect(Build(["3"])).toBe("На вход принимается только число")
-    })
-
-    test('Should return input type error(null)', () => {
-        expect(Build(null)).toBe("На вход принимается только число")
-    })
-
-    test('Should return input type error(undefined)', () => {
-        expect(Build(undefined)).toBe("На вход принимается только число")
-    })
-
-    test('Should return input type error(emptiness)', () => {
-        expect(Build()).toBe("На вход принимается только число")
-    })
-
-})
